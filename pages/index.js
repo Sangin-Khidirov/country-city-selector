646e7a3b03ebcfa14811495aa25935400c843e8c
Sangin Khidirov <khidirovs1999@mail.ru>
2022-10-30 04:36:16 +0500

    Set form and save function for country

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,31 @@ import React, {useEffect, useState} from "react";
 import TextField from '@mui/material/TextField';
 import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import {apiHttpClient} from "../api/endpoints";
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import Checkbox from '@mui/material/Checkbox'
+import Container from "@mui/material/Container"
+import Grid from "@mui/material/Grid"
+import {Button, Typography} from "@mui/material";
 
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 const filterOptions = createFilterOptions({
     matchFrom: 'start',
     stringify: (option) => option,
 });
 
-export default function Home() {
+
+function Home() {
     const [countries, setCountries] = useState([])
-    const [countryCode, setCountryCode] = useState('')
+    const [country, setCountry] = useState({
+        code: '',
+        name: ''
+    })
+    const [savedCountry, setSavedCountry] = useState('')
+    const [cities, setCities] = useState([])
 
     useEffect(() => {
         apiHttpClient.returnCountries().then(countries => {
@@ -23,39 +38,87 @@ export default function Home() {
     }, [])
 
     useEffect(() => {
-        if(countryCode){
-            apiHttpClient.returnCities(countryCode).then(cities => {
+        if(country.code){
+            apiHttpClient.returnCities(country.code).then(cities => {
                 console.log('cities', cities)
+                setCities(cities.country)
             })
         }
-    }, [countryCode])
+    }, [country.code])
 
     const handleChangeCountry = (event, value) => {
         if(value === 'Russia (RU)'){
-            setCountryCode('ru')
+            setCountry({...country, code: 'ru', name: value})
         }
         else if(value === 'United States (US)'){
-            setCountryCode('us')
+            setCountry({...country, code: 'us', name: value})
         }
         else {
-            setCountryCode('fr')
+            setCountry({...country, code: 'fr', name: value})
         }
     }
 
+    const handleSavedCountry = () => {
+        setSavedCountry(country.name)
+    }
+
+
     return (
-        <div >
-            <Autocomplete
-                classes={'customAutoComplete'}
-                id="filter-demo"
-                onChange={handleChangeCountry}
-                options={!countries ? [{label:"Loading...", id:0}] : countries }
-                getOptionLabel={(option) => option}
-                filterOptions={filterOptions}
-                sx={{ width: 300 }}
-                renderInput={(params) =>
-                    <TextField {...params} size={'small'} label="Countries" />
-            }
-            />
-        </div>
+        <Container disableGutters style={{alignItems: 'center'}} maxWidth={false} sx={'fullPageContent'}>
+            <Container sx={'smallForm'}>
+                <Grid container style={{textAlign: 'center'}} spacing={3}>
+                    <Grid item xs={12}>
+                        <Autocomplete
+                            id="countries"
+                            onChange={handleChangeCountry}
+                            options={!countries ? [{label: "Loading...", id: 0}] : countries}
+                            getOptionLabel={(option) => option}
+                            filterOptions={filterOptions}
+                            sx={{width: '100%'}}
+                            renderInput={(params) =>
+                                <TextField {...params} size={'small'} label="Countries"/>
+                            }
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Autocomplete
+                            multiple
+                            id="cities"
+                            options={!cities ? [{label: "Loading...", id: 0}] : cities}
+                            disableCloseOnSelect
+                            getOptionLabel={(option) => option}
+                            renderOption={(props, option, {selected}) => (
+                                <li {...props}>
+                                    <Checkbox
+                                        icon={icon}
+                                        checkedIcon={checkedIcon}
+                                        style={{marginRight: 8}}
+                                        checked={selected}
+                                    />
+                                    {option.title}
+                                </li>
+                            )}
+                            sx={{width: '100%'}}
+                            renderInput={(params) => (
+                                <TextField {...params} size={'small'} label="Cities"/>
+                            )}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button size={'small'} style={{width: '100%'}} variant={'inherit'}
+                            onClick={handleSavedCountry}>
+                            Save
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant={'h6'}>
+                            Country: {savedCountry}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Container>
     )
 }
+
+export default Home
--- src/theme.js
+++ src/theme.js
@@ -3,27 +3,50 @@ import { createTheme } from '@mui/material/styles';
 let theme = createTheme()
 
 theme = createTheme(theme, {
-    breakpoints: {
-        values: {
-            xs: 0,
-            sm: 600,
-            md: 900,
-            lg: 1200,
-            xl: 1600,
-        },
-    },
-
     components:{
-        MuiAutocomplete:{
-            variants:[
+
+        MuiContainer: {
+            variants: [
                 {
-                    props: {classes: 'customAutoComplete'},
-                    style:{
-                        margin: 12
+                    props: { sx: 'fullPageContent' },
+                    style: {
+                        display: 'flex',
+                        justifyContent: 'center',
+                        minHeight: '100vh',
+                        padding: 0,
                     }
-                }
+                },
+                {
+                    props: { sx: 'smallForm' },
+                    style: {
+                        justifyContent: 'center',
+                        position: 'relative',
+                        paddingTop: theme.spacing(4),
+                        paddingBottom: theme.spacing(3),
+                        alignItems: 'center',
+                        boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.5)',
+                        borderRadius: "10px",
+                        backgroundColor: '#FFFFFF',
+                        margin: '16px',
+                        maxWidth: '360px !important'
+                    }
+                },
+
             ]
-        }
+        },
+        MuiButton: {
+            variants: [
+                {
+                    props: { variant: 'inherit' },
+                    style: {
+                        background: 'rgba(211,215,215,0.2)',
+                        ":hover": {
+                            background: '#b5bcbc',
+                        },
+                    },
+                },
+            ],
+        },
     }
 })
 
